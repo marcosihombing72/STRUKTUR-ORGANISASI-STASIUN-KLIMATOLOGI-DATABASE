be999921eb5e1ab8286a3135ebd328fba2f208fa
marcosihombing72 <marcosihombing72@gmail.com>
2025-08-11 09:32:15 +0700

    update

--- src/supabase/supabase.client.ts
+++ src/supabase/supabase.client.ts
@@ -1,13 +1,8 @@
-import * as dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-dotenv.config(); // Pastikan ini dipanggil sebelum akses process.env
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
-if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('SUPABASE_URL dan SUPABASE_SERVICE_ROLE_KEY wajib diatur di file .env');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseServiceKey);
+export const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+);
